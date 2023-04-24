434edea8973ef49288d9e9e46ac49b11c9985f08
godardma <mael.godard@ensta-bretagne.org>
2023-04-24 15:01:58 +0200

    changement de point via trigger

--- helios_ros2/mission_manager.py
+++ helios_ros2/mission_manager.py
@@ -1,5 +1,6 @@
 from sensor_msgs.msg import PointCloud
 from geometry_msgs.msg import Point32,TransformStamped,PointStamped 
+from std_srvs.srv import Trigger
 from tf2_ros import TransformBroadcaster
 
 import rclpy
@@ -26,14 +27,18 @@ class MissionManager(Node):
         self.ref_lamb=deg_to_Lamb(ref[0],ref[1])
         print(self.ref_lamb)
         self.path_publisher = self.create_publisher(PointCloud, 'path', 1000)
+        self.path_done_publisher = self.create_publisher(PointCloud, 'path_done', 1000)
         self.target_publisher = self.create_publisher(PointStamped, 'target', 1000)
+        self.trigger_serv=self.create_service(Trigger, 'trigger', self.trigger_callback)
         self.tf_broadcaster = TransformBroadcaster(self)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         f=open("../workspaceRos2/src/helios_ros2/path/triangle_test.txt")
         lines=f.readlines()
         self.path = PointCloud()
+        self.path_done = PointCloud()
         self.path.header.frame_id="map"
+        self.path_done.header.frame_id="map"
         for line in lines:
             tab=line.split(",")
             lat,lon=float(tab[0]),float(tab[1])
@@ -43,6 +48,7 @@ class MissionManager(Node):
             self.path.points.append(pt)
 
         self.path_publisher.publish(self.path)
+        
 
     def timer_callback(self):
         t = TransformStamped()
@@ -59,12 +65,27 @@ class MissionManager(Node):
 
         self.tf_broadcaster.sendTransform(t)
         self.path_publisher.publish(self.path)
+        self.path_done_publisher.publish(self.path_done)
         pt=PointStamped()
         pt.point.x=self.path.points[0].x
         pt.point.y=self.path.points[0].y
         pt.header.frame_id="map"
         self.target_publisher.publish(pt)
 
+    def trigger_callback(self, request, response):
+        response.success =True
+        if len(self.path.points)>1:
+            old_target=self.path.points[0]
+            self.path.points=self.path.points[1:]
+            self.path_done.points.append(old_target)
+            self.get_logger().info('Changing target')
+            response.message ="Next point"
+        else:
+            self.get_logger().info('Mission finished')
+            response.message ="Finish"
+
+
+        return response
 
 
 def main(args=None):
