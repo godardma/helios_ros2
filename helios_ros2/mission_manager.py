ab3acad3abeee22e4a0ac405c47b1ebb42423f6d
godardma <mael.godard@ensta-bretagne.org>
2023-04-26 11:31:43 +0200

    changement frame

--- helios_ros2/mission_manager.py
+++ helios_ros2/mission_manager.py
@@ -67,7 +67,7 @@ class MissionManager(Node):
         t = TransformStamped()
         t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = 'map'
-        t.child_frame_id = 'frame'
+        t.child_frame_id = 'boat'
         t.transform.translation.x = 0.0
         t.transform.translation.y =0.0
         t.transform.translation.z = 0.0
